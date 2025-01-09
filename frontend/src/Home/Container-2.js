6750996432c7f21162c7d53d4d411b6f0762e7b7
duc Tri <ductri02lt2@gmail.com>
2025-01-09 08:15:28 +0700

    `Removed unused CSS files and updated import paths`

---
+++ frontend/src/Home/Container-2.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+import BlurContainer from "../BlurComponent";
+const Home = () => {
+    return (
+        <>
+            <BlurContainer />
+        </>
+    )
+}
+
+export default Home
