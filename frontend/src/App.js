a51e83dcbd81b45fc827aa5b353535b9c41777f9
Trchien123 <huynhtrungchienltt@gmail.com>
2025-01-09 09:02:51 +0700

    aaa

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,14 @@
 
 import '../src/style/App.css';
+import Menu from './Menu';
 import Home from './Home/Container-2';
+import Container1 from './Container-1';
+  
 function App() {
   return (
     <div className="App">
+      <Menu />
+      <Container1 />
       <Home />
     </div>
   );
---
+++ frontend/src/Container-1.js
@@ -0,0 +1,10 @@
+import './Container-1.css';
+
+function Container1() {
+    return (
+        <div className="container-1">
+        </div>
+    )
+}
+
+export default Container1
