81b68e022dec172980144c1edadcd9996a212ebb
Trchien123 <huynhtrungchienltt@gmail.com>
2025-01-09 08:18:22 +0700

    as

---
+++ frontend/src/Menu.js
@@ -0,0 +1,13 @@
+import './Menu.css';
+
+function Menu() {
+    return (
+        <div className="menu-container">
+            <ul className="menu-elements">
+                <li>Company Name</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Menu;
