7314b03e60a49b2aee1558abefc0e01198a71752
Trchien123 <huynhtrungchienltt@gmail.com>
2025-01-09 16:28:16 +0700

    updatee

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -3,14 +3,26 @@ import './Footer.css';
 function Footer() {
     return (
         <div className="footer-container">
-            <div className="footer-content">
-                <ul>
-                    <li>Dan</li>
-                    <li>Du</li>
-                    <li>Tri</li>
-                    <li>Chien</li>
-                </ul>
+            <div className="waves">
+                <div class="wave" id="wave1"></div>
+                <div class="wave" id="wave2"></div>
+                <div class="wave" id="wave3"></div>
+                <div class="wave" id="wave4"></div>
             </div>
+            <ul className="social-icon">
+                <li><a href=""><ion-icon name="logo-facebook"></ion-icon></a></li>
+                <li><a href=""><ion-icon name="logo-twitter"></ion-icon></a></li>
+                <li><a href=""><ion-icon name="logo-linkedin"></ion-icon></a></li>
+                <li><a href=""><ion-icon name="logo-instagram"></ion-icon></a></li>
+            </ul>
+            <ul className="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Team</a></li>
+                <li><a href="#">Contact</a></li>
+            </ul>
+            <p>Copyright © 2012 - 2025 TermsFeed®. All rights reserved.</p>
         </div>
     )
 }
