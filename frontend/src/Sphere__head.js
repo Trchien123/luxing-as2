713a0dc3edd3af030a6f964408bccc3a04eb4ff6
duc Tri <ductri02lt2@gmail.com>
2025-01-11 15:55:32 +0700

    fix animation

--- frontend/src/Sphere__head.js
+++ frontend/src/Sphere__head.js
@@ -2,11 +2,12 @@ import React from "react";
 import './style/sphere.css';
 
 const Sphere__head = () => {
-    return( 
-        <button class="button">
-            <span class="spearhead">Data: The World in Motion</span>
-            <span class="hover-text">Data: The World in Motion</span>
-        </button>
+    return (
+        <div class="sphere--title">
+
+            <h1 class="sphere--title-content">Data: The World in Motion</h1>
+
+        </div>
     )
 }
 export default Sphere__head;
