97c4a385dd2df73bb2cce826113d57ed7478fbda
Trchien123 <huynhtrungchienltt@gmail.com>
2025-01-16 22:42:49 +0700

    table  dashboard

--- frontend/src/Dashboard/DashTable.js
+++ frontend/src/Dashboard/DashTable.js
@@ -1,11 +1,13 @@
 import React from "react";
+import "../Dashboard_table/dashTable.css";
+import DrawCircle from "../Dashboard_table/drawCircles.js";
 
 const DashTable = () => {
     return (
         <main className="DB-table">
             <h1 className="Dbtable-title">this is the graph</h1>
             <div className="Dbtable-component">
-
+                <DrawCircle />
             </div>
         </main>
     )
