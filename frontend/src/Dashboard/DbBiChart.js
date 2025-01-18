3e69f7d6ea657c2c324ca0f6d7ee7175726bd2bf
duc Tri <ductri02lt2@gmail.com>
2025-01-18 09:50:51 +0700

    complete chart

--- frontend/src/Dashboard/DbBiChart.js
+++ frontend/src/Dashboard/DbBiChart.js
@@ -8,32 +8,72 @@ const BinanceChart = () => {
     useEffect(() => {
         const data = [30, 86, 168, 234, 32, 12, 45];
 
-        const width = 500;
-        const height = 300;
-        const barWidth = width / data.length;
+        const updateChart = () => {
+            const width = chartRef.current.offsetWidth;
+            const height = chartRef.current.offsetHeight;
+            const barWidth = width / data.length;
 
-        // Clear the previous SVG if it exists
-        d3.select(chartRef.current).select("svg").remove();
+            // Clear the previous SVG if it exists
+            d3.select(chartRef.current).select("svg").remove();
+            d3.select(chartRef.current).select(".tooltip").remove();
+            const svg = d3.select(chartRef.current)
+                .append("svg")
+                .attr("width", width)
+                .attr("height", height);
 
-        const svg = d3.select(chartRef.current)
-            .append("svg")
-            .attr("width", "100%")
-            .attr("height", "100%");
 
-        svg.selectAll("rect")
-            .data(data)
-            .enter()
-            .append("rect")
-            .attr("width", barWidth - 1)
-            .attr("height", d => d)
-            .attr("x", (d, i) => i * barWidth)
-            .attr("y", d => height - d)
-            .attr("fill", "teal");
+            const tooltip = d3.select(chartRef.current)
+                .append("div")
+                .attr("class", "tooltip")
+
+                .style("opacity", 0)
+            const mouseover = function (d) {
+                tooltip
+                    .style("opacity", 1)
+                d3.select(this)
+                    .style("stroke", "black")
+                    .style("opacity", 1)
+            }
+            const mousemove = function (event, d) {
+                const [x, y] = d3.pointer(event, this);
+                tooltip
+                    .html("Value:" + d)
+                    .style("left", (x + 50) + "px")
+                    .style("top", (y - 300) + "px")
+
+            }
+            const mouseleave = function (d) {
+                tooltip
+                    .style("opacity", 0)
+                d3.select(this)
+                    .style("stroke", "none")
+                    .style("opacity", 0.8)
+            }
+            svg.selectAll("rect")
+                .data(data)
+                .enter()
+                .append("rect")
+                .attr("width", barWidth - 1)
+                .attr("height", d => (d / Math.max(...data)) * height)
+                .attr("x", (d, i) => i * barWidth)
+                .attr("y", d => height - ((d / Math.max(...data)) * height))
+                .attr("fill", "teal")
+                .on("mouseover", mouseover)
+                .on("mousemove", mousemove)
+                .on("mouseleave", mouseleave)
+        };
+
+        updateChart();
+
+        window.addEventListener("resize", updateChart);
+
+        return () => {
+            window.removeEventListener("resize", updateChart);
+        };
     }, []); // Add an empty dependency array to ensure the effect is only run once
 
     return (
         <div id="my_dataviz" ref={chartRef} className="Bo-chart"></div>
     )
 };
-
 export default BinanceChart
