f2055adcd6dab2e5f5b3d52ae080c678bbf8c657
duc Tri <ductri02lt2@gmail.com>
2025-02-08 22:06:03 +0700

    real final changes

--- frontend/src/Dashboard/DbNavbar.js
+++ frontend/src/Dashboard/DbNavbar.js
@@ -1,6 +1,25 @@
 import React from "react";
 import DbItem from "./DbItem";
 import { Link } from "react-router-dom";
+const iconsElement = [
+    {
+        icon: (<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24">
+            <path fill-rule="evenodd" d="M11.293 3.293a1 1 0 0 1 1.414 0l6 6 2 2a1 1 0 0 1-1.414 1.414L19 12.414V19a2 2 0 0 1-2 2h-3a1 1 0 0 1-1-1v-3h-2v3a1 1 0 0 1-1 1H7a2 2 0 0 1-2-2v-6.586l-.293.293a1 1 0 0 1-1.414-1.414l2-2 6-6Z" clip-rule="evenodd" />
+        </svg>),
+        content: "Dashboard"
+    },
+    {
+        icon: (
+            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <path opacity="0.5" d="M2.75 2C2.75 1.58579 2.41421 1.25 2 1.25C1.58579 1.25 1.25 1.58579 1.25 2V12.0574C1.24999 14.3658 1.24998 16.1748 1.43975 17.5863C1.63399 19.031 2.03933 20.1711 2.93414 21.0659C3.82895 21.9607 4.96897 22.366 6.41371 22.5603C7.82519 22.75 9.63423 22.75 11.9426 22.75H22C22.4142 22.75 22.75 22.4142 22.75 22C22.75 21.5858 22.4142 21.25 22 21.25H12C9.62178 21.25 7.91356 21.2484 6.61358 21.0736C5.33517 20.9018 4.56445 20.5749 3.9948 20.0052C3.42514 19.4355 3.09825 18.6648 2.92637 17.3864C2.75159 16.0864 2.75 14.3782 2.75 12V2Z" fill="#ffff"></path> <path d="M19.5875 7.46644C19.8451 7.14207 19.791 6.67029 19.4666 6.4127C19.1422 6.1551 18.6705 6.20924 18.4129 6.53362L15.2948 10.46C15.0496 10.7689 14.8887 10.9708 14.7562 11.1163C14.6265 11.2586 14.5657 11.2989 14.538 11.3137C14.3272 11.4264 14.0754 11.4319 13.8599 11.3285C13.8316 11.3149 13.7691 11.2772 13.6334 11.1407C13.4946 11.0011 13.3251 10.8064 13.0667 10.5085L13.0505 10.4899C12.8127 10.2157 12.6098 9.98191 12.4309 9.80187C12.2448 9.61472 12.0414 9.44013 11.7894 9.31921C11.143 9.00901 10.3875 9.02544 9.75521 9.36345C9.50875 9.49521 9.3131 9.67848 9.13539 9.87354C8.96444 10.0612 8.77195 10.3036 8.54622 10.5878L5.4127 14.5336C5.15511 14.858 5.20924 15.3298 5.53361 15.5874C5.85798 15.845 6.32976 15.7908 6.58735 15.4665L9.70554 11.54C9.9508 11.2312 10.1117 11.0292 10.2442 10.8837C10.3739 10.7415 10.4347 10.7011 10.4624 10.6863C10.6731 10.5736 10.925 10.5681 11.1405 10.6715C11.1688 10.6851 11.2313 10.7228 11.367 10.8593C11.5057 10.9989 11.6752 11.1937 11.9337 11.4915L11.9499 11.5102C12.1877 11.7843 12.3906 12.0181 12.5695 12.1982C12.7555 12.3853 12.959 12.5599 13.2109 12.6808C13.8573 12.991 14.6129 12.9746 15.2452 12.6366C15.4917 12.5048 15.6873 12.3215 15.865 12.1265C16.0359 11.9388 16.2284 11.6964 16.4541 11.4122L19.5875 7.46644Z" fill="#ffff"></path> </g></svg>
+        ),
+        content: "Table"
+    },
+    {
+        icon: (<svg viewBox="0 0 48 48" xmlns="http://www.w3.org/2000/svg" fill="#ffffff"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>news</title> <g id="Layer_2" data-name="Layer 2"> <g id="invisible_box" data-name="invisible box"> <rect width="48" height="48" fill="none"></rect> </g> <g id="icons_Q2" data-name="icons Q2"> <path d="M40,8V40H8V8H40m2-4H6A2,2,0,0,0,4,6V42a2,2,0,0,0,2,2H42a2,2,0,0,0,2-2V6a2,2,0,0,0-2-2Z"></path> <path d="M34,30H14a2,2,0,0,0,0,4H34a2,2,0,0,0,0-4Z"></path> <path d="M34,22H28a2,2,0,0,0,0,4h6a2,2,0,0,0,0-4Z"></path> <path d="M34,14H28a2,2,0,0,0,0,4h6a2,2,0,0,0,0-4Z"></path> <rect x="12" y="14" width="10" height="12" rx="2" ry="2"></rect> </g> </g> </g></svg>),
+        content: "News"
+    }
+
+]
 
 const DbNavbar = ({ setShowBar, avatar }) => {
     const handleShowBar = () => {
@@ -42,7 +61,19 @@ const DbNavbar = ({ setShowBar, avatar }) => {
             <h1 className="navbar--name">Address</h1>
 
             <div className="content">
-                <Link to={'/Dashboard'} className="navbar--name--url">
+                {
+                    iconsElement.map(({ icon, content }, index) => {
+                        const url = content === "Dashboard" ? "" : content
+                        return (
+                            <Link key={index} to={`/Dashboard/${url}`} state={avatar} className="navbar--name--url">
+                                <DbItem content={content} icon={icon} />
+                            </Link>
+                        )
+                    }
+
+                    )
+                }
+                {/* <Link to={'/Dashboard'} className="navbar--name--url">
                     <DbItem content={"Dashboard"} />
                 </Link>
 
@@ -52,7 +83,7 @@ const DbNavbar = ({ setShowBar, avatar }) => {
                 </Link>
                 <Link to={'/Dashboard/News'} className="navbar--name--url">
                     <DbItem content={"News"} />
-                </Link>
+                </Link> */}
 
 
             </div>
--- frontend/src/Dashboard/DbNews.js
+++ frontend/src/Dashboard/DbNews.js
@@ -1,7 +1,7 @@
-import React, { useState, useEffect, useRef } from "react";
-import img1 from "../asset/db-background1.jpg"
-import img2 from "../asset/db-background2.jpg"
-import img3 from "../asset/db-background3.jpg"
+import React from "react";
+import img1 from "../asset/DbNews-image-1.jpg"
+import img2 from "../asset/DbNews-image-2.jpg"
+import img3 from "../asset/DbNews-image-3.jpg"
 
 import DNImageSlider from "./DnImageSlider";
 import Footer from "../Footer";
@@ -11,10 +11,10 @@ const IMAGES = [
     { url: img3, alt: "Image 3" },
 ]
 const STICKYCONTENT = [
-    { url: img1, title: "Thanh Hoa", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eaque sed ad eum consectetur, rerum debitis commodi ut aut quae omnis, dolores molestias unde tenetur nihil. Eveniet consectetur vel provident?" },
-    { url: img1, title: "Thanh Hoa", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eaque sed ad eum consectetur, rerum debitis commodi ut aut quae omnis, dolores molestias unde tenetur nihil. Eveniet consectetur vel provident?" },
+    { url: img1, title: "Da Nang", content: "A leading cryptocurrency exchange has announced a partnership with a major traditional bank, allowing users to seamlessly transfer funds between fiat and digital currencies. This collaboration aims to enhance user experience and promote mainstream adoption of cryptocurrencies, bridging the gap between traditional finance and digital assets." },
+    { url: img2, title: "Ho Chi Minh", content: "Ethereum’s long-awaited 2.0 upgrade has been successfully implemented, transitioning the network from proof-of-work to proof-of-stake. This upgrade is expected to improve scalability, reduce energy consumption, and enhance security. Developers are optimistic that this transition will attract more users and investors to the Ethereum ecosystem." },
 
-    { url: img1, title: "Thanh Hoa", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto eaque sed ad eum consectetur, rerum debitis commodi ut aut quae omnis, dolores molestias unde tenetur nihil. Eveniet consectetur vel provident?" }
+    { url: img3, title: "Swinburne", content: "After a slowdown, the NFT market is experiencing a resurgence in interest and sales. Major brands and artists are re-entering the space, launching new collections and collaborations. Analysts suggest that innovative use cases and improved platforms are driving this renewed enthusiasm among collectors and investors alike." }
 ]
 
 const DashNews = () => {
@@ -31,8 +31,7 @@ const DashNews = () => {
             <section className="DN-section3">
                 <div className="DN-section3-container">
                     <h1 className="title">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vel dolores earum perferendis dolore nihil harum sint hic! Animi, veniam nobis!
-                    </h1>
+                        NFT sales have surged, with collectors eager for unique digital art pieces, revitalizing interest in the non-fungible token market.                        </h1>
                     <button className="DN-section3-container-btn">More Details</button>
                 </div>
             </section>
@@ -73,7 +72,7 @@ const DNStickyContent = ({ data, index }) => {
 
         }}>
             <div className="Dn-sticky-container-img">
-                <img src={data.url} />
+                <img src={data.url} alt="data url" />
             </div>
             <div className="Dn-sticky-container-content">
                 <h1>{data.title}</h1>
