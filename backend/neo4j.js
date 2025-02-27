745404c7d64a1f116f57ae1917a98a89f3d97fa6
dannguyen0309 <169075433+dannguyen0309@users.noreply.github.com>
2025-02-28 00:02:21 +0700

    update neo4j path

--- backend/neo4j.js
+++ backend/neo4j.js
@@ -1,7 +1,6 @@
-require('dotenv').config()
+require("dotenv").config({ path: "./api.env" });
 
-
-var neo4j = require('neo4j-driver');
+var neo4j = require("neo4j-driver");
 
 // URI examples: 'neo4j://localhost', 'neo4j+s://xxx.databases.neo4j.io'
 const URI = process.env.NEO4J_URI;
@@ -15,8 +14,6 @@ const PASSWORD = process.env.NEO4J_PASSWORD;
 //     try {
 //         driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD))
 //         const serverInfo = await driver.getServerInfo()
-
-
 
 //         console.log('Connection established')
 //         console.log(serverInfo)
@@ -39,49 +36,45 @@ const PASSWORD = process.env.NEO4J_PASSWORD;
 // })();
 
 async function runNeo4Query(query) {
-    const driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD))
-    let session;
+  const driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD));
+  let session;
 
-    try {
-        session = driver.session()
-        const serverInfo = await driver.getServerInfo()
-        console.log('connection established')
-        console.log(serverInfo)
+  try {
+    session = driver.session();
+    const serverInfo = await driver.getServerInfo();
+    console.log("connection established");
+    console.log(serverInfo);
 
-        const result = await session.run(query)
-        const records = result.records.map(record => {
-            const path = record.get('p')
-            const relationship = record.get('r')
-            return {
-                path: path,
-                relationship: relationship.properties
-            }
-        })
-        return records
-
-    } catch (err) {
-        console.error(`Conenction error \n${err}Cause: ${err.cause}`)
-        throw err;
-    } finally {
-        if (session) {
-            await session.close()
-        }
-        driver.close()
+    const result = await session.run(query);
+    const records = result.records.map((record) => {
+      const path = record.get("p");
+      const relationship = record.get("r");
+      return {
+        path: path,
+        relationship: relationship.properties,
+      };
+    });
+    return records;
+  } catch (err) {
+    console.error(`Conenction error \n${err}Cause: ${err.cause}`);
+    throw err;
+  } finally {
+    if (session) {
+      await session.close();
     }
-
+    driver.close();
+  }
 }
 
 (async () => {
-    const query = 'MATCH p=()-[r:`CREATE TRANSACTION`]->() RETURN p,r ;';
-    try {
-        const records = await runNeo4Query(query)
-        records.forEach(record => {
-            console.log('Path:', record.path)
-            console.log('Relationship:', record.relationship)
-
-        })
-
-    } catch (err) {
-        console.error(err)
-    }
+  const query = "MATCH p=()-[r:`CREATE TRANSACTION`]->() RETURN p,r ;";
+  try {
+    const records = await runNeo4Query(query);
+    records.forEach((record) => {
+      console.log("Path:", record.path);
+      console.log("Relationship:", record.relationship);
+    });
+  } catch (err) {
+    console.error(err);
+  }
 })();
